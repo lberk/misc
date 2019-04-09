be485d5bd51292a375475de0cf118df553e2a252
Matthias Wessendorf <mwessend@redhat.com>
2019-04-09 22:06:51 +0200

    Kube version configurable

--- setup.sh
+++ setup.sh
@@ -20,6 +20,7 @@ serving_version="0.4.1"
 eventing_version="0.4.1"
 eventing_sources_version="0.4.1"
 istio_version="v0.4.0"
+kube_version="v1.12.1"
 
 MEMORY="$(minikube config view | awk '/memory/ { print $3 }')"
 CPUS="$(minikube config view | awk '/cpus/ { print $3 }')"
@@ -37,7 +38,7 @@ header_text "Using Knative Eventing Version:         ${eventing_version}"
 header_text "Using Knative Eventing Sources Version: ${eventing_sources_version}"
 header_text "Using Istio Version:                    ${istio_version}"
 
-minikube start --memory="${MEMORY:-12288}" --cpus="${CPUS:-4}" --kubernetes-version=v1.12.1 --vm-driver="${DRIVER:-kvm2}" --disk-size="${DISKSIZE:-30g}" --extra-config=apiserver.enable-admission-plugins="LimitRanger,NamespaceExists,NamespaceLifecycle,ResourceQuota,ServiceAccount,DefaultStorageClass,MutatingAdmissionWebhook"
+minikube start --memory="${MEMORY:-12288}" --cpus="${CPUS:-4}" --kubernetes-version=${kube_version} --vm-driver="${DRIVER:-kvm2}" --disk-size="${DISKSIZE:-30g}" --extra-config=apiserver.enable-admission-plugins="LimitRanger,NamespaceExists,NamespaceLifecycle,ResourceQuota,ServiceAccount,DefaultStorageClass,MutatingAdmissionWebhook"
 
 header_text "Strimzi install"
 kubectl create namespace kafka
