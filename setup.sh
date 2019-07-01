363edafc21fc5272ac596a0d873610ef951f09b8
Matthias Wessendorf <mwessend@redhat.com>
2019-07-01 12:10:53 +0200

    Latest 0.7.0 (removing of sources), and istio lean

--- setup.sh
+++ setup.sh
@@ -16,10 +16,9 @@ else
 fi
 
 strimzi_version=`curl https://github.com/strimzi/strimzi-kafka-operator/releases/latest |  awk -F 'tag/' '{print $2}' | awk -F '"' '{print $1}' 2>/dev/null`
-serving_version="0.4.1"
-eventing_version="0.4.1"
-eventing_sources_version="0.4.1"
-istio_version="v0.4.0"
+serving_version="v0.7.0"
+eventing_version="v0.7.0"
+istio_version="1.1.7"
 kube_version="v1.12.1"
 
 MEMORY="$(minikube config view | awk '/memory/ { print $3 }')"
@@ -36,7 +35,6 @@ header_text "Using Kubernetes Version:               ${kube_version}"
 header_text "Using Strimzi Version:                  ${strimzi_version}"
 header_text "Using Knative Serving Version:          ${serving_version}"
 header_text "Using Knative Eventing Version:         ${eventing_version}"
-header_text "Using Knative Eventing Sources Version: ${eventing_sources_version}"
 header_text "Using Istio Version:                    ${istio_version}"
 
 minikube start --memory="${MEMORY:-12288}" --cpus="${CPUS:-4}" --kubernetes-version="${kube_version}" --vm-driver="${DRIVER:-kvm2}" --disk-size="${DISKSIZE:-30g}" --extra-config=apiserver.enable-admission-plugins="LimitRanger,NamespaceExists,NamespaceLifecycle,ResourceQuota,ServiceAccount,DefaultStorageClass,MutatingAdmissionWebhook"
@@ -54,11 +52,11 @@ kubectl -n kafka apply -f "https://raw.githubusercontent.com/strimzi/strimzi-kaf
 header_text "Waiting for Strimzi to become ready"
 sleep 5; while echo && kubectl get pods -n kafka | grep -v -E "(Running|Completed|STATUS)"; do sleep 5; done
 
-header_text "Setting up istio"
-kubectl apply --filename "https://github.com/knative/serving/releases/download/${istio_version}/istio-crds.yaml" &&
-    curl -L "https://github.com/knative/serving/releases/download/${istio_version}/istio.yaml" \
-        | sed 's/LoadBalancer/NodePort/' \
-        | kubectl apply --filename -
+header_text "Setting up Istio lean"
+curl -L "https://raw.githubusercontent.com/knative/serving/${serving_version}/third_party/istio-${istio_version}/istio-lean.yaml" \
+    | sed 's/LoadBalancer/NodePort/' \
+    | kubectl apply --filename -
+
 
 # Label the default namespace with istio-injection=enabled.
 header_text "Labeling default namespace w/ istio-injection=enabled"
@@ -67,7 +65,7 @@ header_text "Waiting for istio to become ready"
 sleep 5; while echo && kubectl get pods -n istio-system | grep -v -E "(Running|Completed|STATUS)"; do sleep 5; done
 
 header_text "Setting up Knative Serving"
-curl -L "https://raw.githubusercontent.com/openshift-knative/knative-serving-operator/master/deploy/resources/knative-serving-${serving_version}.yaml" \
+curl -L "https://github.com/knative/serving/releases/download/${serving_version}/serving.yaml" \
   | sed 's/LoadBalancer/NodePort/' \
   | kubectl apply --filename -
 
@@ -76,9 +74,7 @@ sleep 5; while echo && kubectl get pods -n knative-serving | grep -v -E "(Runnin
 
 
 header_text "Setting up Knative Eventing"
-kubectl apply --filename https://raw.githubusercontent.com/openshift-knative/knative-eventing-operator/master/deploy/resources/knative-eventing-${eventing_sources_version}.yaml
-kubectl apply --filename https://raw.githubusercontent.com/openshift-knative/knative-eventing-operator/master/deploy/resources/knative-eventing-sources-${eventing_sources_version}.yaml
+kubectl apply --filename https://github.com/knative/eventing/releases/download/${eventing_version}/release.yaml
 
 header_text "Waiting for Knative Eventing to become ready"
 sleep 5; while echo && kubectl get pods -n knative-eventing | grep -v -E "(Running|Completed|STATUS)"; do sleep 5; done
-sleep 5; while echo && kubectl get pods -n knative-sources | grep -v -E "(Running|Completed|STATUS)"; do sleep 5; done
