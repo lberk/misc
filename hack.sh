00e5052359aa7dd1e316dd63f470607e6170dee8
Lukas Berk <lberk@redhat.com>
2019-12-04 17:14:14 -0500

    Install strimzi in a function call

--- hack.sh
+++ hack.sh
@@ -122,6 +122,20 @@ function install_upstream_serving() {
 
 }
 
+function install_strimzi() {
+    mk_setup_env
+    header_text "Strimzi install"
+    kubectl create namespace kafka
+    curl -L "https://github.com/strimzi/strimzi-kafka-operator/releases/download/${strimzi_version}/strimzi-cluster-operator-${strimzi_version}.yaml" \
+        | sed 's/namespace: .*/namespace: kafka/' \
+        | kubectl -n kafka apply -f -
+
+    header_text "Applying Strimzi Cluster file"
+    kubectl -n kafka apply -f "https://raw.githubusercontent.com/strimzi/strimzi-kafka-operator/${strimzi_version}/examples/kafka/kafka-persistent-single.yaml"
+    header_text "Waiting for Strimzi to become ready"
+    sleep 5; while echo && kubectl get pods -n kafka | grep -v -E "(Running|Completed|STATUS)"; do sleep 5; done
+
+}
 function mk_stop() {
     minikube delete
 }
@@ -139,16 +153,9 @@ function mk_start() {
     header_text "Waiting for core k8s services to initialize"
     sleep 5; while echo && kubectl get pods -n kube-system | grep -v -E "(Running|Completed|STATUS)"; do sleep 5; done
 
-    header_text "Strimzi install"
-    kubectl create namespace kafka
-    curl -L "https://github.com/strimzi/strimzi-kafka-operator/releases/download/${strimzi_version}/strimzi-cluster-operator-${strimzi_version}.yaml" \
-        | sed 's/namespace: .*/namespace: kafka/' \
-        | kubectl -n kafka apply -f -
+    sleep 5; while echo && kubectl get pods -n kube-system | grep -v -E "(Running|Completed|STATUS)"; do sleep 5; done
 
-    header_text "Applying Strimzi Cluster file"
-    kubectl -n kafka apply -f "https://raw.githubusercontent.com/strimzi/strimzi-kafka-operator/${strimzi_version}/examples/kafka/kafka-persistent-single.yaml"
-    header_text "Waiting for Strimzi to become ready"
-    sleep 5; while echo && kubectl get pods -n kafka | grep -v -E "(Running|Completed|STATUS)"; do sleep 5; done
+    install_strimzi
 
     setup_istio
 
